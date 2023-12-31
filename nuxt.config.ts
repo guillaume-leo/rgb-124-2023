4d4fff54b43e766068a2f006d8235fae290e970a
GUILLAUME QUET <quet.guillaume.leo@gmail.com>
2023-12-31 13:37:28 +0000

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/rgb-124-2023/',
+    buildAssetsDir: 'assets'
+  },
   ssr:false,
   modules: ["@tresjs/nuxt"],
   devtools: { enabled: true }
